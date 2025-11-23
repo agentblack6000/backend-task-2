1d79f933a3668a2ae75b4f0ef99252515790e8f7
agentblack6000 <halan.raghav@gmail.com>
2025-11-23 18:06:45 +0530

    Add functionality for the admin to view footfall at each station

--- passengers/admin.py
+++ passengers/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.db import models
+from django.utils.html import format_html
 from .models import Station, Passenger, Ticket, Connection, Line
 # Register your models here.
 
-admin.site.register(Station)
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    list_display = ["name", "ticket_count", "tickets_list"]
+
+    def ticket_count(self, obj):
+        """Returns the total number of tickets starting or ending at this station"""
+        count = Ticket.objects.filter(
+            models.Q(start_station=obj) | models.Q(destination=obj)
+        ).count()
+        return count
+    ticket_count.short_description = "Number of Tickets"
+
+    def tickets_list(self, obj):
+        """Returns a bullet point list of tickets"""
+        tickets = Ticket.objects.filter(
+            models.Q(start_station=obj) | models.Q(destination=obj)
+        )
+        if not tickets.exists():
+            return "-"
+        
+        return ", ".join([
+            f"{t.passenger.user.username} ({t.start_station.name} to {t.destination.name})"
+            for t in tickets
+        ])
+    tickets_list.short_description = "Ticket Data"
+
+
 admin.site.register(Passenger)
 admin.site.register(Ticket)
 admin.site.register(Connection)
